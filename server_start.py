8a7bd8d7286736d5882b33402f24cb40d741b147
aditya.baskar <aditya.baskar@gmail.com>
2020-03-28 18:49:57 +0530

    Changed the tcp server to a threaded server

--- server_start.py
+++ server_start.py
@@ -1,11 +1,16 @@
 import SocketServer
 import json
 import sys
+import threading
+import urllib2
 
 def load_routes():
 	global routes
 	with open("routes.config") as json_data:
 		routes = json.load(json_data)
+
+class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
+    pass
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
 	def parse_request(self):
@@ -18,6 +23,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 		for line in lines:
 			if (len(line.strip()) == 0):
 				body_flag = True
+				continue
 			if body_flag == False:
 				if line.split(" ")[0].lower() == "get" or line.split(" ")[0].lower() == "post" \
 				or line.split(" ")[0].lower() == "put":
@@ -36,7 +42,9 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 
 			elif header["type"].lower() != "get":
 				body += line + "\n"
-		self.parsed = {"header": header, "body": body}
+		print header
+		print body
+		self.parsed = {"header": header, "body": body.strip()}
 
 	def validate_reqest(self):
 		global routes
@@ -47,7 +55,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 		return False
 
 	def execute_request(self):
-		global user_file, routes
+		global routes
 		try:
 			user_handlers = __import__(user_file)
 		except:
@@ -73,10 +81,9 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 			ret_val = self.execute_request()
 		self.request.sendall(str(ret_val))
 
-
 if __name__ == "__main__":
 	load_routes()
-	HOST, PORT = "localhost", 9999
+	HOST, PORT = "localhost", 9998
 	try:
 		global user_file
 		if sys.argv[1].find(".") >= 0:
@@ -91,5 +98,13 @@ if __name__ == "__main__":
 		data = json.load(json_data)
 		HOST = str(data["host"])
 		PORT = data["port"]
-	server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
-	server.serve_forever()
+	server = ThreadedTCPServer((HOST, PORT), MyTCPHandler)
+	server_thread = threading.Thread(target=server.serve_forever)
+	server_thread.daemon = True
+	server_thread.start()
+
+	while raw_input("type exit to stop server: ") != "exit":
+		print "server running"
+
+	server.shutdown()
+	server.server_close()
