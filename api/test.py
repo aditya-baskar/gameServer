7a80f7992b591f3af408dc66b8d7b359f87bffd5
aditya.baskar <aditya.baskar@gmail.com>
2020-03-29 10:03:26 +0530

    Working api

---
+++ api/test.py
@@ -0,0 +1,5 @@
+def get(req_obj):
+	return "<html><body><h1>Hello World!</h1></body></html>"
+
+def post(req_obj):
+	return "done"
--- server_start.py
+++ server_start.py
@@ -3,6 +3,8 @@ import json
 import sys
 import threading
 import urllib2
+import importlib
+from requests.models import Response
 
 def load_routes():
 	global routes
@@ -14,7 +16,6 @@ class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
 	def parse_request(self):
-		print self.data
 		lines = self.data.split("\n")
 		header = {}
 		body = ""
@@ -29,7 +30,7 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 				or line.split(" ")[0].lower() == "put":
 					parts = map(str.strip, line.split(" "))
 					header["version"] = parts[2]
-					header["type"] = parts[0]
+					header["method"] = parts[0]
 					if parts[0].lower() == "get" and parts[1].find("?") >= 0:
 						header["url"] = parts[1].split("?", 1)[0]
 						body = parts[1].split("?")[1]
@@ -40,33 +41,31 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 					pair = line.split(":", 1)
 					header[pair[0].strip()] = pair[1].strip()
 
-			elif header["type"].lower() != "get":
+			elif header["method"].lower() != "get":
 				body += line + "\n"
-		print header
-		print body
+		header["url"] = urllib2.unquote(header["url"])
 		self.parsed = {"header": header, "body": body.strip()}
 
 	def validate_reqest(self):
 		global routes
 		header = self.parsed["header"]
 		for route in routes:
-			if route["url"] == header["url"] and header["type"] == route["method"]:
+			if route["url"].lower() == header["url"].lower() and header["method"].lower() == route["method"].lower():
 				return True
 		return False
 
 	def execute_request(self):
 		global routes
-		try:
-			user_handlers = __import__(user_file)
-		except:
-			print "error with provided file"
-			return None
 		req_header = self.parsed["header"]
+		func_name = req_header["method"]
+		module = ""
 		for obj in routes:
 			if obj["url"] == req_header["url"]:
-				func_name = obj["handler"];
+				if (obj.has_key("handler")):
+					func_name = obj["handler"]
+				module = obj["url"].split("/", 1)[1].replace("/", ".")
 		try:
-			method = getattr(user_handlers, func_name)
+			method = getattr(importlib.import_module(module), func_name)
 		except:
 			print "error when loading function"
 			return None
@@ -77,34 +76,26 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
 		self.parse_request()
 		ret_val = None
 		if self.validate_reqest():
-			print "here"
 			ret_val = self.execute_request()
-		self.request.sendall(str(ret_val))
+			resp_obj = self.create_response(ret_val)
+
+		self.request.sendall(resp_obj)
+
+
+	def create_response(self,data):
+		resp_string = self.parsed["header"]["version"] + " 200 OK\n" + \
+		"Content-Type: text/html\nContent-Length: " + str(len(str(data))) + "\n\n" + data 
+		return resp_string
+
+
 
 if __name__ == "__main__":
 	load_routes()
 	HOST, PORT = "localhost", 9998
-	try:
-		global user_file
-		if sys.argv[1].find(".") >= 0:
-			user_file = sys.argv[1].split(".")[0]
-		else:
-			user_file = sys.argv[1]
-		print user_file
-	except:
-		print "no user file given"
-		sys.exit()
+	
 	with open("server.config") as json_data:
 		data = json.load(json_data)
 		HOST = str(data["host"])
 		PORT = data["port"]
-	server = ThreadedTCPServer((HOST, PORT), MyTCPHandler)
-	server_thread = threading.Thread(target=server.serve_forever)
-	server_thread.daemon = True
-	server_thread.start()
-
-	while raw_input("type exit to stop server: ") != "exit":
-		print "server running"
-
-	server.shutdown()
-	server.server_close()
+	server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+	server.serve_forever()
